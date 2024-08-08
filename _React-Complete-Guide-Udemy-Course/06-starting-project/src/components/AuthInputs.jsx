38ca68aa7a0766fd779a6831cc40fcf174c59eb9
Sheldon Pasciak <sheldon@pasciak.com>
2024-08-08 11:10:16 -0600

    Styled components

--- _React-Complete-Guide-Udemy-Course/06-starting-project/src/components/AuthInputs.jsx
+++ _React-Complete-Guide-Udemy-Course/06-starting-project/src/components/AuthInputs.jsx
@@ -1,12 +1,76 @@
-import { useState } from 'react';
+import { useState } from "react";
+
+import { styled } from "styled-components";
+
+import Button from "./Button.jsx";
+import CustomInput from "./Input.jsx";
+
+// Tagged template, javascript feature
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals#tagged_templates
+const ControlContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  margin-bottom: 1.5rem;
+  border: 2px solid red;
+`;
+
+// const Label = styled.label`
+//   display: block;
+//   margin-bottom: 0.5rem;
+//   font-size: 0.75rem;
+//   font-weight: 700;
+//   letter-spacing: 0.1em;
+//   text-transform: uppercase;
+//   color: ${({ $invalid }) => {
+//     return $invalid ? "#f87171" : "#6b7280";
+//   }};
+// `;
+
+// const Input = styled.input`
+//   width: 100%;
+//   padding: 0.75rem 1rem;
+//   line-height: 1.5;
+//   background-color: #d1d5db;
+//   color: #374151;
+//   border: 1px solid transparent;
+//   border-radius: 0.25rem;
+//   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+
+//   background-color: ${({ $invalid }) => {
+//     return $invalid ? "#fed2d2" : "#d1d5db";
+//   }};
+
+//   border-color: ${({ $invalid }) => {
+//     return $invalid ? "#f73f3f" : "transparent";
+//   }};
+
+//   color: ${({ $invalid }) => {
+//     return $invalid ? "#ef4444" : "#374151";
+//   }};
+// `;
+
+// const Button = styled.button`
+//   padding: 1rem 2rem;
+//   font-weight: 600;
+//   text-transform: uppercase;
+//   border-radius: 0.25rem;
+//   color: #1f2937;
+//   background-color: #f0b322;
+//   border-radius: 6px;
+//   border: none;
+//   &:hover {
+//     background-color: #f0920e;
+//   }
+// `;
 
 export default function AuthInputs() {
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [enteredPassword, setEnteredPassword] = useState('');
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredPassword, setEnteredPassword] = useState("");
   const [submitted, setSubmitted] = useState(false);
 
   function handleInputChange(identifier, value) {
-    if (identifier === 'email') {
+    if (identifier === "email") {
       setEnteredEmail(value);
     } else {
       setEnteredPassword(value);
@@ -17,36 +81,35 @@ export default function AuthInputs() {
     setSubmitted(true);
   }
 
-  const emailNotValid = submitted && !enteredEmail.includes('@');
+  const emailNotValid = submitted && !enteredEmail.includes("@");
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
     <div id="auth-inputs">
-      <div className="controls">
-        <p>
-          <label>Email</label>
-          <input
-            type="email"
-            className={emailNotValid ? 'invalid' : undefined}
-            onChange={(event) => handleInputChange('email', event.target.value)}
-          />
-        </p>
-        <p>
-          <label>Password</label>
-          <input
-            type="password"
-            className={passwordNotValid ? 'invalid' : undefined}
-            onChange={(event) =>
-              handleInputChange('password', event.target.value)
-            }
-          />
-        </p>
-      </div>
+      <ControlContainer className="controls">
+        <CustomInput
+          label="Email"
+          type="email"
+          invalid={emailNotValid}
+          // className={emailNotValid ? "$invalid" : undefined}
+          onChange={(event) => handleInputChange("email", event.target.value)}
+        />
+        <CustomInput
+          label="Password"
+          invalid={passwordNotValid}
+          type="password"
+          // className={passwordNotValid ? "$invalid" : undefined}
+          onChange={(event) =>
+            handleInputChange("password", event.target.value)
+          }
+        />
+      </ControlContainer>
+      <p>Some Text</p>
       <div className="actions">
         <button type="button" className="text-button">
           Create a new account
         </button>
-        <button className='button' onClick={handleLogin}>Sign In</button>
+        <Button onClick={handleLogin}>Sign In</Button>
       </div>
     </div>
   );
--- _React-Complete-Guide-Udemy-Course/06-starting-project/src/components/Header.jsx
+++ _React-Complete-Guide-Udemy-Course/06-starting-project/src/components/Header.jsx
@@ -1,11 +1,80 @@
-import logo from '../assets/logo.png';
+import logo from "../assets/logo.png";
+import { styled } from "styled-components";
+
+const StyledHeader = styled.header`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 2rem;
+  margin-bottom: 2rem;
+
+  & img {
+    object-fit: contain;
+    margin-bottom: 2rem;
+    width: 11rem;
+    height: 11rem;
+  }
+
+  & h1 {
+    font-size: 1.5rem;
+    font-weight: 600;
+    letter-spacing: 0.4em;
+    text-align: center;
+    text-transform: uppercase;
+    color: #9a3412;
+    font-family: "Pacifico", cursive;
+    margin: 0;
+  }
+
+  & p {
+    text-align: center;
+    color: #a39191;
+    margin: 0;
+  }
+
+  @media (min-width: 768px) {
+    margin-bottom: 4rem;
+
+    & h1 {
+      font-size: 2.25rem;
+    }
+  }
+`;
+
+// const Header = styled("header", {
+//   display: "flex",
+//   alignItems: "center",
+//   gap: "1rem",
+//   padding: "1rem",
+//   backgroundColor: "#f9fafb",
+//   borderBottom: "1px solid #d1d5db",
+//   "& img": {
+//     width: "3rem",
+//     height: "3rem",
+//   },
+//   "& h1": {
+//     fontSize: "1.5rem",
+//     fontWeight: "700",
+//     color: "#111827",
+//   },
+//   "& p": {
+//     fontSize: "0.875rem",
+//     color: "#6b7280",
+//   },
+// });
+
+// NOTE: Using CSS Modules for class scoping of CSS styles
+// import classes from "./Header.module.css";
 
 export default function Header() {
   return (
-    <header>
+    <StyledHeader>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
-    </header>
+      <p /*className={classes.paragraph} */>
+        A community of artists and art-lovers.
+      </p>
+    </StyledHeader>
   );
 }
