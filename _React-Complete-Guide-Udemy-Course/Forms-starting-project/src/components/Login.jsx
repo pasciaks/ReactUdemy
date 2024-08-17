55dd8befca05d9dacd954430f482dcb0752c383e
Sheldon Pasciak <sheldon@pasciak.com>
2024-08-17 08:35:05 -0600

    Forms

---
+++ _React-Complete-Guide-Udemy-Course/Forms-starting-project/src/components/Login.jsx
@@ -0,0 +1,30 @@
+export default function Login() {
+  function handleSubmit(e) {
+    e.preventDefault();
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    console.log(email, password);
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Login</h2>
+
+      <div className="control-row">
+        <div className="control no-margin">
+          <label htmlFor="email">Email</label>
+          <input required id="email" type="email" name="email" />
+        </div>
+
+        <div className="control no-margin">
+          <label htmlFor="password">Password</label>
+          <input id="password" type="password" name="password" />
+        </div>
+      </div>
+
+      <p className="form-actions">
+        <button className="button button-flat">Reset</button>
+        <button className="button">Login</button>
+      </p>
+    </form>
+  );
+}
