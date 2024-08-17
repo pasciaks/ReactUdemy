50fb667f8f3fae7a5b526b420f6a0cfea4e35bbc
Sheldon Pasciak <sheldon@pasciak.com>
2024-08-17 09:35:35 -0600

    Forms complete

--- _React-Complete-Guide-Udemy-Course/17 Forms User Input/10-building-using-reusable-input-cmp/src/App.jsx
+++ _React-Complete-Guide-Udemy-Course/17 Forms User Input/10-building-using-reusable-input-cmp/src/App.jsx
@@ -1,6 +1,5 @@
 import Header from "./components/Header.jsx";
-import Login from "./components/Login.jsx";
-import Signup from "./components/Signup.jsx";
+import Login from "./components/StateLogin.jsx";
 
 function App() {
   return (
@@ -8,7 +7,6 @@ function App() {
       <Header />
       <main>
         <Login />
-        <Signup />
       </main>
     </>
   );
--- _React-Complete-Guide-Udemy-Course/Forms-starting-project/src/components/Login.jsx
+++ _React-Complete-Guide-Udemy-Course/Forms-starting-project/src/components/Login.jsx
@@ -1,9 +1,34 @@
+import React, { useState, useRef } from "react";
+
 export default function Login() {
-  function handleSubmit(e) {
-    e.preventDefault();
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const eRef = useRef();
+  const pRef = useRef();
+
+  function onChange(event) {
+    const { name, value } = event.target;
+    if (name === "email") {
+      setEmail(value);
+    } else {
+      setPassword(value);
+    }
+  }
+
+  function onReset() {
+    console.log(eRef.current.value);
+    eRef.current.value = "";
+    console.log(pRef.current.value);
+    pRef.current.value = "";
+    //setEmail("");
+    //setPassword("");
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
     console.log(email, password);
+    onReset();
   }
   return (
     <form onSubmit={handleSubmit}>
@@ -12,17 +37,34 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input required id="email" type="email" name="email" />
+          <input
+            ref={eRef}
+            onChange={onChange}
+            required
+            id="email"
+            type="email"
+            name="email"
+            value={email}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            ref={pRef}
+            onChange={onChange}
+            id="password"
+            type="password"
+            name="password"
+            value={password}
+          />
         </div>
       </div>
 
       <p className="form-actions">
-        <button className="button button-flat">Reset</button>
+        <button type="button" onClick={onReset} className="button button-flat">
+          Reset
+        </button>
         <button className="button">Login</button>
       </p>
     </form>
